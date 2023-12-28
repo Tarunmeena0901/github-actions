5cecc5988e0c60a627aa864e80e24db9ca552e53
Ta.run <97682967+Tarunmeena0901@users.noreply.github.com>
2023-12-28 17:17:59 +0530

    Update check_changed_files.py

--- .github/workflows/check_changed_files.py
+++ .github/workflows/check_changed_files.py
@@ -16,7 +16,7 @@ def get_current_branch():
 def get_base_branch():
     try:
         # Run git command to get the base branch
-        command = "git ls-remote origin main | awk '{print $1}'"
+        command = "git rev-parse HEAD"
         base_branch = os.popen(command).read().strip()
         print(base_branch)
         return base_branch
